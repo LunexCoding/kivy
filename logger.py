da54b45688240e2e084461e41412eb2981689c6f
LunexCoding <lunexcoding@vk.com>
2023-12-05 03:57:39 +0700

    Update

--- logger.py
+++ logger.py
@@ -1,9 +1,14 @@
+from kivy.utils import platform
+
 from pathlib import Path
 from Logger import Logger
 
 
 _FILENAME = "log.md"
-_DIR = Path("logs")
+if platform == "android":
+    _DIR = Path("/sdcard/emulated/0/Download")
+else:
+    _DIR = Path("logs")
 
 
 logger = Logger()
